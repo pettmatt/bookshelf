1e39c3eacec1c7644c7564d09e6a5fc0581f673d
Petteri Mattila <petterimattila@protonmail.com>
2025-01-21 12:23:37 +0200

    Camera settings changed + strict mode added for development

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,23 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
+import { StrictMode } from 'react'
 import Experience from './Experience.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas
-        shadows
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ 4, 2, 6 ]
-        } }
-    >
-        <Experience />
-    </Canvas>
+	<StrictMode>
+		<Canvas
+			shadows
+			camera={ {
+				fov: 45,
+				near: 0.1,
+				far: 300,
+				position: [ 0, 5, 12 ]
+			} }
+		>
+			<Experience />
+		</Canvas>
+	</StrictMode>
 )
