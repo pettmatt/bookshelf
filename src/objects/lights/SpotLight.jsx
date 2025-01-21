ef0340e494f2ccba33a05ade07cb1844e04e2e64
Petteri Mattila <petterimattila@protonmail.com>
2025-01-21 12:23:12 +0200

    Spotlight moved to its own component

---
+++ src/objects/lights/SpotLight.jsx
@@ -0,0 +1,31 @@
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+
+export default function SpotLight(props) {
+	const spotLightRef = useRef()
+	const visualizedLight = useRef()
+
+	useFrame(() => {
+		if (visualizedLight.current && spotLightRef.current) {
+		  // Update light position if needed
+		  visualizedLight.current.position.copy(spotLightRef.current.position);
+		}
+	})
+
+	function increaseYValue(locationArray) {
+		if (!Array.isArray(locationArray)) return
+
+		const newLocation = [...locationArray]
+		const setY = newLocation[1]
+		newLocation[1] = (setY < 3) ? 3 : setY
+		return newLocation
+	}
+
+	return <>
+		<spotLight castShadow ref={ spotLightRef } position={ increaseYValue(props.position) || [0, 5, 0] } angle={ 2 } penumbra={ 0.5 } intensity={ 1.5 } />
+		<mesh ref={ visualizedLight }>
+			<sphereGeometry args={ [ 0.2 ] } />
+			<meshBasicMaterial wireframe={ true } color="yellow" opacity={ 0.3 } transparent />
+		</mesh>
+	</>
+}
