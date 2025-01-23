bdd00da7a40fea147ed685a2054a5d2690f3739a
Petteri Mattila <petterimattila@protonmail.com>
2025-01-23 16:44:12 +0200

    Handling click event on instanced meshes correctly

--- src/objects/Book.jsx
+++ src/objects/Book.jsx
@@ -1,5 +1,5 @@
 import { shaderMaterial, useTexture } from '@react-three/drei'
-import { extend, useFrame } from '@react-three/fiber'
+import { extend, useFrame, useThree } from '@react-three/fiber'
 import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 import fragmentShader from '../shaders/book/fragment.glsl'
@@ -15,7 +15,42 @@ const material = shaderMaterial(
 
 extend({ material })
 
-export default function Book({ books, material, count, position }) {
+export default function Book({ books, material, count, position, enableClick }) {
+	const { raycaster, camera, mouse } = useThree()
+
+	function selectBook() {
+		if(!books.current)
+			return
+
+		raycaster.setFromCamera(mouse, camera)
+		const intersections = raycaster.intersectObject(books.current)
+
+		if (intersections.length > 0) {
+			const instanceId = intersections[0].instanceId
+			console.log("Clicked", instanceId, intersections[0])
+
+			// For some reason below doesn't produce anything. Probably because the instance doesn't have material set correctly
+			// const color = new THREE.Color(Math.random(), Math.random(), Math.random())
+			// books.current.setColorAt(instanceId, color)
+			// books.current.instanceColor.needsUpdate = true
+
+			// Change position
+			const matrix = new THREE.Matrix4()
+			books.current.getMatrixAt(instanceId, matrix)
+
+			const position = new THREE.Vector3()
+			const rotation = new THREE.Quaternion()
+			const scale = new THREE.Vector3()
+			matrix.decompose(position, rotation, scale)
+
+			position.add(new THREE.Vector3(0, 0, 2))
+
+			matrix.compose(position, rotation, scale)
+			books.current.setMatrixAt(instanceId, matrix)
+			books.current.instanceMatrix.needsUpdate = true
+		}
+	}
+
     // const bakedTexture = useTexture('../model/textureName.jpg')
     // bakedTexture.flipY = false
 
@@ -30,7 +65,10 @@ export default function Book({ books, material, count, position }) {
     // })
 
     return (
-		<instancedMesh ref={ books } castShadow receiveShadow args={ [null, null, count] } position={ position }>
+		<instancedMesh ref={ books } castShadow receiveShadow 
+			args={ [null, null, count] } position={ position }
+			onClick={ selectBook }
+		>
 			<boxGeometry />
 			<meshStandardMaterial color="orange" />
 		</instancedMesh>
